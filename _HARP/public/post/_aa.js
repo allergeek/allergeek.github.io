f30d80cc76408c761de664140b8b3e43ca971f55
Alice <allergeek@gmail.com>
2017-04-25 06:59:00 -0700

    post template upd

--- _HARP/public/post/_aa.js
+++
@@ -1,6 +0,0 @@
-console.log('aa.js log me')
-//render('p psd')
-
-var bbb = [
-  { "notes": [ "Choose an easy route <b>for your first</b> run. Plan your route around neighborhood streets you're already familiar with.",
-  "You don't have to buy running gear to start running. Put on something you feel comfortable in. Pick your favorite hoodie or t-shirt over everything else." ] } ]
