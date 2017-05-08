73e3a276225ed30201c6289d4bdd9b826959fce2
Alice <allergeek@gmail.com>
2017-05-08 09:59:39 -0700

    print styles and function

---
+++ _HARP/public/ref.js
@@ -0,0 +1,16 @@
+if (true) {
+  // alert('?');
+}
+
+<!--
+       get every h2 tag content
+       in a certain class div
+       take first 30 chars
+       and build urls with it
+       wrap every h2 element in an anchor
+-->
+
+// append
+// span
+// class="counter"
+// title="references"
