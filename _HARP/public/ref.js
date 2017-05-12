a5bc2cf40d696978d83aa968aa284e42ace44a73
Alice <allergeek@gmail.com>
2017-05-12 13:15:46 -0700

    some progress with comments overlay

--- _HARP/public/ref.js
+++ _HARP/public/ref.js
@@ -33,8 +33,10 @@ function clean(n) {
 
     myArray = document.getElementsByClassName("page-body")[0].getElementsByTagName('h2');
     for (i = 0, l = myArray.length; i < l; i++) {
-      textArray += myArray[i].innerHTML;
-      myArray[i].innerHTML = "<a>[i]</a>";
+      // myArray[i].append(`<span class="counter" title="references">NNN</span>`);
+      textArray[i] = myArray[i].innerHTML;
+      myArray[i].innerHTML = `${textArray[i]}`;
+      // REWRITE APPENDING ? CHECK
     }
 
 
@@ -51,6 +53,48 @@ function clean(n) {
   // wrap
   // in url[i]
 })();
+
+function meme() {
+  var arr = document.getElementsByClassName("page-body")[0].getElementsByTagName('h2');
+  for (i = 0, l = arr.length; i < l; i++) {
+    arr[i].onclick = mew;
+  }
+}
+
+function mew(event) {
+  var sel = getSelection().toString();
+  var haveSel = sel.length > 0;
+  if(!haveSel) {
+    // location.href = "/";
+    llo();
+  }
+}
+
+function mww(event) {
+  var element = document.getElementById("ttt");
+  element.outerHTML = "";
+}
+
+function llo() {
+  var div = document.createElement('div');
+  var div2 = document.createElement('div');
+  var btn = document.createElement('button');
+  var large = div;
+  var cc = div2;
+  large.id = "ttt";
+  large.style.cssText = 'position:fixed;top:0;left:0;width:100%;height:100%;opacity:1;z-index:999;background:#444;paddding:100px;';
+  cc.style.cssText = 'position:relative;width:calc(100%-100px);height:calc(100%);margin:100px;';
+  btn.style.cssText = 'position:fixed;top:20px;left:20px;width:20px;height:20px;';
+  large.appendChild(cc);
+  large.appendChild(btn);
+  cc.id = "overlay-content";
+  document.body.appendChild(large);
+  // console.log(large);
+  document.getElementById("overlay-content").innerHTML='<object type="text/html" data="how-the-brain-is-capable-of-growing-new-cells-ref/the-diet-modulates-memory-and-.html" style="width: 100%;height:100%;"></object>';
+  btn.onclick = mww;
+}
+
+// llo();
 
 // some posts don't have references
 // hence need a flag in json
